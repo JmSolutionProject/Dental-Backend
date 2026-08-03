08c0a6d0c1755801785f6944174f7f7f078a856d
BrennisC <brennis.castro@unas.edu.pe>
2026-08-03 15:46:17 -0500

    probar la api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('heathl')
+  @Get('health')
   getHealth(): string {
     return this.appService.getHealth();
   }
